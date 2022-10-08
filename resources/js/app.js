dda8d21ee59dbca11d301e21c489405c76208ea5
ekopba29 <42029591+ekopba29@users.noreply.github.com>
2022-10-08 11:10:22 +0700

    first commit

---
+++ resources/js/app.js
@@ -0,0 +1,6 @@
+import { createApp } from "vue";
+import App from '../views_vue/App.vue'
+
+import 'tw-elements';
+
+createApp(App).mount('#app')
---
+++ resources/views_vue/App.vue
@@ -0,0 +1,11 @@
+<template>
+   <div class="bg-red-500 p-4 w-full">
+    hai
+   </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
